75cbd586f6c5dfc3d1d49b9ebf8cc3421bf100b4
ABarthe <a.barthe@proton.me>
2025-04-10 13:29:13 +0200

    Hello World Program

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+//Class definition
+public class Main {
+
+    //Main function, start of program
+    public static void main(String[] args) {
+
+        //Output of Hello World in one line
+        System.out.println("Hello World!");
+
+    }
+}
