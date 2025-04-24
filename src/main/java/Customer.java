97db6eeecea6c8f197e6772cf3fe2bdedd663e28
Oskar Oetken <oskaroetken@web.de>
2025-04-24 10:36:32 +0200

    Create class Customer

---
+++ src/main/java/Customer.java
@@ -0,0 +1,55 @@
+import java.time.LocalDate;
+
+public class Customer {
+
+    private long id;
+    private String username;
+    private String email;
+    private LocalDate dateofbirth;
+
+    public Customer(long id, String username, String email, LocalDate birth) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.dateofbirth = birth;
+    }
+
+    public Customer(Customer customer) {
+        this.id = customer.id;
+        this.username = customer.username;
+        this.email = customer.email;
+        this.dateofbirth = customer.dateofbirth;
+    }
+
+    public long getId(){
+        return this.id;
+    }
+    public void setId(long id){
+        this.id = id;
+    }
+    public String getUsername(){
+        return this.username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public LocalDate getDateofbirth(){
+        return this.dateofbirth;
+    }
+    public void setDateofbirth(LocalDate birth){
+        this.dateofbirth = birth;
+    }
+
+    @Override
+    public String toString(){
+        return "Kunde: [id: " + id + ", username: " + username + ", email: " + email + ", dateofbirth: " + dateofbirth + "]";
+    }
+
+
+}
