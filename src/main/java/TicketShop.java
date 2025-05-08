fbd9b37cefb6f737a6344a1a7879a4284c4950f8
ABarthe <a.barthe@proton.me>
2025-05-08 13:31:50 +0200

    Add ticki shoppi

--- src/main/java/TicketShop.java
+++ src/main/java/TicketShop.java
@@ -1,11 +1,33 @@
+import interfaces.CustomerServiceInterface;
+import interfaces.EventServiceInterface;
+import interfaces.TicketServiceInterface;
+import services.CustomerService;
+import services.EventService;
+import services.TicketService;
+
 public class TicketShop {
 
+    private CustomerServiceInterface customerService;
+    private EventServiceInterface eventService;
+    private TicketServiceInterface ticketService;
 
+    public TicketShop() {
+        this.customerService = new CustomerService();
+        this.eventService = new EventService();
+        this.ticketService = new TicketService();
+    }
 
+    public CustomerServiceInterface getCustomerService() {
+        return customerService;
+    }
 
+    public EventServiceInterface getEventService() {
+        return eventService;
+    }
 
-
-
+    public TicketServiceInterface getTicketService() {
+        return ticketService;
+    }
 
 
 }
